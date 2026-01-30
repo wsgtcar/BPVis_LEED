547b3ae34a4497a91fba9deecaf3940e1dc2ab90
wsgtcar <rodrigo.carvalho@wernersobek.com>
2026-01-30 10:50:08 +0100

    Update WSGTLEED_1.0.0.py

--- WSGTLEED_1.0.0.py
+++ WSGTLEED_1.0.0.py
@@ -80,7 +80,7 @@ RESPONSIBLE_OPTIONS = [
     "Owner", "Project Manager", "Operator", "Architect", "Landscape Architect", "Interior Architect",
     "MEP Engineer", "Structural Engineer", "Electrical Engineer", "Sustainability Consultant", "Building Physics",
     "Energy Engineer","Facility Manager", "Comissioning Authority", "Infrastructure Engineer",
-    "Simulation Expert", "Accoustic Engineer", "Lighting Designer", "Contractor"
+    "Simulation Expert", "Accoustic Engineer", "Lighting Designer", "Contractor", "Facade Designer"
 ]
 RESPONSIBLE_DEFAULTS = ["Owner", "Sustainability Consultant", "Architect"]
 
@@ -2862,4 +2862,5 @@ with sidebar:
     st.caption("*email:* rodrigo.carvalho@wernersobek.com")
     st.caption("*Tel* +49.40.6963863-14")
     st.caption("*Mob* +49.171.964.7850")
+
 
